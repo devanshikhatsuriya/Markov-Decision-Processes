d3c23dca65c9fab42173ff4213305f43972cde25
Devanshi Khatsuriya <devanshikhatsuriya18@gmail.com>
2021-11-24 11:11:22 +0530

    added T and R functions

--- main.py
+++ main.py
@@ -20,17 +20,20 @@ class Grid:
                             [".", ":", ".", "|", ".", ":", ".", ":", "."],
                             ["R", ":", ".", "|", ".", ":", ".", ":", "G"],]
             self.right_walls = [[False for i in range(5)] for i in range(5)] # True for grid cells that have walls on their right side
-            for cell in [(0,0), (0,1), (2,0), (2,1), (1,3), (1,4)]:
+            for cell in [(0,0), (1,0), (0,2), (1,2), (3,1), (4,1)]:
                 self.right_walls[cell[0]][cell[1]] = True
             self.left_walls = [[False for i in range(5)] for i in range(5)] # True for grid cells that have walls on their left side
-            for cell in [(1,0), (1,1), (3,0), (3,1), (2,3), (2,4)]:
+            for cell in [(0,1), (1,1), (0,3), (1,3), (3,2), (4,2)]:
                 self.left_walls[cell[0]][cell[1]] = True
             # note that we model only vertical walls as there are no horizontal walls
-            self.depots = {"R": [0,4], "B": [3,0], "G": [4,4], "Y": [0,0]}
+            self.depots = {"R": (4,0), "B": (0,3), "G": (4,4), "Y": (0,0)}
+            # cell locations are represented as (row_no, col_no) = (y_coord., x_coord.)
         elif type == 2:
             self.size = (10, 10)        
 
 class TaxiDomain:
+
+    nav_actions = ["N", "E", "S", "W"]
 
     def __init__(self, grid):
         self.grid = grid
@@ -39,95 +42,156 @@ class TaxiDomain:
         depot_names = ["R", "B", "G", "Y"]
         selected_depots = random.sample(depot_names, 2)
         # print(selected_depots)
-        self.passenger_loc = grid.depots[selected_depots[0]]
+        passenger_loc = grid.depots[selected_depots[0]]
+        self.dest_depot = selected_depots[1]
         self.dest_loc = grid.depots[selected_depots[1]]
-        self.taxi_loc = [random.randint(0,4), random.randint(0,4)] # random.randint(0, 4) returns a random integer in [0, 4]
+        taxi_loc = (random.randint(0,4), random.randint(0,4)) # random.randint(0, 4) returns a random integer in [0, 4]
         
-        self.passenger_in_taxi = False
-        # state is defined as (self.taxi_loc, self.passenger_loc, self.passenger_in_taxi)
+        passenger_in_taxi = False
+        self.state = (taxi_loc, passenger_loc, passenger_in_taxi)
 
         # stats that are maintained
         self.last_action = ""
-        self.last_result = ""
         self.last_reward = ""
 
-    def take_action(self, a):
+    def T(self, s, a):
         '''
-            INPUT: action a represents action to be taken by the taxi agent 
-                    a is in {"N", "E", "S", "W", "PICKUP", "PUTDOWN"}
-            RETURNS: updates self.state and returns reward according to transition and reward model
+            INPUT: state s and action a to be taken on this state
+            RETURNS: a dictionary of probability distribution of next state s' given s, a
+                    probs: {s1': T(s, a, s1'), s2': T(s, a, s2') ...} 
         '''
-        self.last_action = a
-        self.last_result = ""
-        self.last_reward = -1
-        nav_actions = ["N", "E", "S", "W"]
+        probs = {}
+        taxi_loc, passenger_loc, passenger_in_taxi = s
 
-        if a in nav_actions:
-            sample = random.uniform(0,1)
-            if sample >= 0.15: # move in direction of a with probability 0.85
-                final_direction = a
+        if a in TaxiDomain.nav_actions:
+            # initialize probablility of 0.05 in all directions
+            if taxi_loc[0] < 4: # if not at the top most location in grid
+                probs[((taxi_loc[0]+1, taxi_loc[1]), passenger_loc, passenger_in_taxi)] = 0.05
             else:
-                nav_actions.remove(a)
-                # print(nav_actions)
-                final_direction = random.choice(nav_actions)
-            self.last_result = final_direction
+                if s in probs:
+                    probs[s] += 0.05
+                else:
+                    probs[s] = 0.05
+            if taxi_loc[1] < 4 and not self.grid.right_walls[taxi_loc[0]][taxi_loc[1]]: # if not at the right most location in grid and no wall on right side 
+                probs[((taxi_loc[0], taxi_loc[1]+1), passenger_loc, passenger_in_taxi)] = 0.05
+            else:
+                if s in probs:
+                    probs[s] += 0.05
+                else:
+                    probs[s] = 0.05
+            if taxi_loc[0] > 0: # if not at the bottom most location in grid
+                probs[((taxi_loc[0]-1, taxi_loc[1]), passenger_loc, passenger_in_taxi)] = 0.05
+            else:
+                if s in probs:
+                    probs[s] += 0.05
+                else:
+                    probs[s] = 0.05
+            if taxi_loc[1] > 0 and not grid.left_walls[taxi_loc[0]][taxi_loc[1]]: # if not at the left most location in grid and no wall on left side 
+                probs[((taxi_loc[0], taxi_loc[1]-1), passenger_loc, passenger_in_taxi)] = 0.05
+            else:
+                if s in probs:
+                    probs[s] += 0.05
+                else:
+                    probs[s] = 0.05
 
-            # execute final_action
-            if final_direction == "N":
-                if self.taxi_loc[0] < 4: # if not at the top most location in grid
-                    self.taxi_loc[0] += 1
-            elif final_direction == "E":
-                if self.taxi_loc[1] < 4 and not self.grid.right_walls[self.taxi_loc[0]][self.taxi_loc[1]]: # if not at the right most location in grid and no wall on right side 
-                    self.taxi_loc[1] += 1
-            elif final_direction == "S":
-                if self.taxi_loc[0] > 0: # if not at the bottom most location in grid
-                    self.taxi_loc[0] -= 1
+            # probability 0.85 in direction of a
+            if a == "N":
+                if taxi_loc[0] < 4: # if not at the top most location in grid
+                    probs[((taxi_loc[0]+1, taxi_loc[1]), passenger_loc, passenger_in_taxi)] = 0.85
+                else:
+                    if s in probs:
+                        probs[s] += 0.85
+                    else:
+                        probs[s] = 0.85
+            elif a == "E":
+                if taxi_loc[1] < 4 and not self.grid.right_walls[taxi_loc[0]][taxi_loc[1]]: # if not at the right most location in grid and no wall on right side 
+                    probs[((taxi_loc[0], taxi_loc[1]+1), passenger_loc, passenger_in_taxi)] = 0.85
+                else:
+                    if s in probs:
+                        probs[s] += 0.85
+                    else:
+                        probs[s] = 0.85
+            elif a == "S":
+                if taxi_loc[0] > 0: # if not at the bottom most location in grid
+                    probs[((taxi_loc[0]-1, taxi_loc[1]), passenger_loc, passenger_in_taxi)] = 0.85
+                else:
+                    if s in probs:
+                        probs[s] += 0.85
+                    else:
+                        probs[s] = 0.85
             else:
-                if self.taxi_loc[1] > 0 and not self.grid.left_walls[self.taxi_loc[0]][self.taxi_loc[1]]: # if not at the left most location in grid and no wall on left side 
-                    self.taxi_loc[1] -= 1
-            if self.passenger_in_taxi:
-                self.passenger_loc = self.taxi_loc
+                if taxi_loc[1] > 0 and not grid.left_walls[taxi_loc[0]][taxi_loc[1]]: # if not at the left most location in grid and no wall on left side 
+                    probs[((taxi_loc[0], taxi_loc[1]-1), passenger_loc, passenger_in_taxi)] = 0.85
+                else:
+                    if s in probs:
+                        probs[s] += 0.85
+                    else:
+                        probs[s] = 0.85
+
+        else: # a is PICKUP or PUTDOWN
+            if taxi_loc == passenger_loc:
+                if a == "PUTDOWN":
+                    probs[(taxi_loc, passenger_loc, False)] = 1
+                elif a == "PICKUP":
+                    probs[(taxi_loc, passenger_loc, True)] = 1
+            else:
+                probs[s] = 1
+        
+        return probs
+
+    def R(self, s, a):
+        '''
+            INPUT: state s and action a to be taken on this state
+            RETURNS: reward for this (s, a) tuple
+                    (note that reward does not depend on s', i.e., R(s, a, s') = R(s, a) for all s')
+        '''
+        taxi_loc, passenger_loc, passenger_in_taxi = s
+
+        if a in TaxiDomain.nav_actions:
             return -1
-
         else:
-            if self.taxi_loc == self.passenger_loc:
-                if a == "PUTDOWN" and self.passenger_in_taxi and self.taxi_loc == self.dest_loc:
-                    self.last_result = "FINISH"
-                    self.last_reward = 20
+            if taxi_loc == passenger_loc:
+                if a == "PUTDOWN" and passenger_in_taxi and taxi_loc == self.dest_loc:
                     return 20
-                if a == "PICKUP":
-                    if not self.passenger_in_taxi:
-                        self.last_result = "PICKUP"
-                        self.passenger_in_taxi = True
-                    else:
-                        self.last_result = "NONE"
-                else: # a == "PUTDOWN"
-                    if not self.passenger_in_taxi:
-                        self.last_result = "NONE"
-                    else:
-                        self.last_result = "PUTDOWN"
-                return -1
+                else:
+                    return -1
             else:
-                self.last_result = "FAIL"
-                self.last_reward = -10
                 return -10
+
+    def take_action(self, s, a):
+        '''
+            INPUT: state s and action a taken on this state
+                    a is in {"N", "E", "S", "W", "PICKUP", "PUTDOWN"}
+            RETURNS: samples the next state s' given s, a and returns the tuple (s', R(s, a))
+        '''
+        next_state_probs = self.T(s, a)
+        reward = self.R(s, a)
+
+        # sample a next state s' from the probability distribution
+        next_state = random.choices(list(next_state_probs.keys()), weights=next_state_probs.values(), k=1)[0]
+        self.last_action = a
+        self.last_reward = reward
+
+        return (next_state, reward)
 
     def print_state(self):
         '''
             prints the grid, locations of taxi and passenger and relevent stats
         '''
+        taxi_loc, passenger_loc, passenger_in_taxi = self.state
+
         grid = [row[:] for row in self.grid.array]
         message = ""
-        if self.taxi_loc == self.passenger_loc:
+        if taxi_loc == passenger_loc:
             if passenger_in_taxi:
-                grid[self.taxi_loc[0]][2*self.taxi_loc[1]] = "O"
+                grid[taxi_loc[0]][2*taxi_loc[1]] = "O"
                 message = "Passenger in taxi at O"
             else:
-                grid[self.taxi_loc[0]][2*self.taxi_loc[1]] = "T"
+                grid[taxi_loc[0]][2*taxi_loc[1]] = "T"
                 message = "Passenger and taxi at T"
         else:
-            grid[self.taxi_loc[0]][2*self.taxi_loc[1]] = "T"
-            grid[self.passenger_loc[0]][2*self.passenger_loc[1]] = "P"
+            grid[taxi_loc[0]][2*taxi_loc[1]] = "T"
+            grid[passenger_loc[0]][2*passenger_loc[1]] = "P"
             message = "Passenger at P and taxi at T"
 
         print()
@@ -136,10 +200,9 @@ class TaxiDomain:
             for point in row:
                 print(point, end=" ")
             print()
-        print(f"Action: {self.last_action}  Result: {self.last_result}  Reward: {self.last_reward}")
+        print(f"Action: {self.last_action}  Reward: {self.last_reward}  Dest: {self.dest_depot}")
         print(message)
         
-    # def partA_1b():
     def value_iteration(self, discount, epsilon):
         delta=0
         #states list? Transition T?
@@ -162,17 +225,17 @@ class TaxiDomain:
         return u
 
 
-    # def partA_1c():        
+    #      
 
 if __name__ == "__main__":
     grid = Grid(1)
     tdp = TaxiDomain(grid)
     tdp.print_state()
-    tdp.take_action("N")
+    tdp.state, reward = tdp.take_action(tdp.state, "N")
     tdp.print_state()
-    tdp.take_action("N")
+    tdp.state, reward = tdp.take_action(tdp.state, "N")
     tdp.print_state()
-    tdp.take_action("E")
+    tdp.state, reward = tdp.take_action(tdp.state, "E")
     tdp.print_state()
 
     # partA_1b()
